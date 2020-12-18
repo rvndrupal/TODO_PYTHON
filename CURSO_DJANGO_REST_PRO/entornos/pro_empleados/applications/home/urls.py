3448676adad64e4a18e6348cca737bf234b86c4a
Rodrigo Igor Villanueva Nieto <rodrigo.villanueva.c@senasica.gob.mx>
2020-12-18 17:36:30 -0600

    Carga de los primeros modelos

--- CURSO_DJANGO_REST_PRO/entornos/pro_empleados/applications/home/urls.py
+++ CURSO_DJANGO_REST_PRO/entornos/pro_empleados/applications/home/urls.py
@@ -8,4 +8,6 @@ from . import views
 urlpatterns = [
     path('prueba/', views.PruebaView.as_view()),
     path('prueba2/', views.PruebaListView.as_view()),
+    path('lista_prueba3/', views.ListaPrueba3.as_view()),
+    path('add/', views.Prueba3CreateView.as_view()),
 ]
--- CURSO_DJANGO_REST_PRO/entornos/pro_empleados/applications/home/views.py
+++ CURSO_DJANGO_REST_PRO/entornos/pro_empleados/applications/home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-from django.views.generic import TemplateView, ListView 
+from django.views.generic import TemplateView, ListView, CreateView 
+
+#modelos
+from .models import *
 
 
 
@@ -12,5 +15,18 @@ class  PruebaListView(ListView):
     template_name = "home/prueba2.html"
     context_object_name='numeros'
     queryset= ['0','10','20','30']
+    
+class ListaPrueba3(ListView):
+    template_name = "home/lista_prueba3.html"
+    context_object_name='lp3'
+    model=Prueba3
+    
+
+class Prueba3CreateView(CreateView):
+    model = Prueba3
+    template_name = "home/add.html"
+    fields=['titulo','subtitulo','cantidad']
+
+    
     
 
