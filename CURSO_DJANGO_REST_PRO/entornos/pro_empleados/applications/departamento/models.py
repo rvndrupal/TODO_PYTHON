5e5ab2970c6f4800cf9563100a81804ba8ad84de
Rodrigo Igor Villanueva Nieto <rodrigo.villanueva.c@senasica.gob.mx>
2020-12-18 19:34:19 -0600

    Aprendiendo más sobre los modelos

--- CURSO_DJANGO_REST_PRO/entornos/pro_empleados/applications/departamento/models.py
+++ CURSO_DJANGO_REST_PRO/entornos/pro_empleados/applications/departamento/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Departamento(models.Model):
+    name = models.CharField('Nombre', max_length=50, unique=True) #el campo sea unico
+    #short_name = models.CharField('Nombre corto', max_length=50, blank=True, editable=False) #Que el campo no es Requerido, no se ve por el editable
+    short_name = models.CharField('Nombre corto', max_length=50, blank=True, editable=True) #Que el campo no es Requerido
+    anulate = models.BooleanField('Anulado', default=False)
+       
+    
+    def __str__(self):
+        return self.name +'-'+ self.short_name
+    
